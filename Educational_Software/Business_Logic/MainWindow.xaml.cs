71c8bbacb68244eb56c5ad8b7ecf876373910d82
PavlisKostas <98098077+kpavlis@users.noreply.github.com>
2025-09-15 02:44:47 +0300

    Improvements

--- Educational_Software/Business_Logic/MainWindow.xaml.cs
+++ Educational_Software/Business_Logic/MainWindow.xaml.cs
@@ -308,7 +308,7 @@ namespace Educational_Software
             {
 
                 double dheight = height / 1.35;
-                double dwidth = width / 1.2;
+                double dwidth = width / 2.4;
 
                 height = (int)dheight;
                 width = (int)dwidth;
